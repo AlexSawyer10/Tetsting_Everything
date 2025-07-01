ba2b56e39d8414aa8b8da5fe9c3dd3dcaa5b672a
AlexSawyer10 <asawyer37@southhills.edu>
2025-07-01 17:52:35 -0400

    Showed what I will be doing

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace New_Repo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("I am going to be making different branches and" +
+                          " random commits and I'm going to see what happens");
+        
+    }
+}
